cc840130974e91c8b1767525578af7211991e5ea
Rudolph-Miller <chopsticks.tk.ppfm@gmail.com>
2016-09-22 18:40:36 +0900

    Add Input and Preview components

--- index.ios.js
+++ index.ios.js
@@ -8,24 +8,16 @@ import React, { Component } from 'react';
 import {
   AppRegistry,
   StyleSheet,
-  Text,
   View
 } from 'react-native';
+import { Input, Preview } from './src/components';
 
 class MarkdownMemo extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+        <Input />
+        <Preview />
       </View>
     );
   }
@@ -34,20 +26,10 @@ class MarkdownMemo extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
     backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  }
 });
 
 AppRegistry.registerComponent('MarkdownMemo', () => MarkdownMemo);
---
+++ src/components/Input/index.ios.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+export default class Input extends Component {
+  render() {
+    return (
+      <View>
+        <Text>
+          Input
+        </Text>
+      </View>
+    );
+  }
+}
