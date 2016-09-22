1cdad244fde98fd5e1721fd335fe8cad3e12c944
Rudolph-Miller <chopsticks.tk.ppfm@gmail.com>
2016-09-22 19:34:42 +0900

    Add NavigationIOS

--- index.ios.js
+++ index.ios.js
@@ -4,32 +4,77 @@
  * @flow
  */
 
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 import {
   AppRegistry,
   StyleSheet,
-  View
+  View,
+  ScrollView,
+  NavigatorIOS
 } from 'react-native';
 import { Input, Preview } from './src/components';
 
-class MarkdownMemo extends Component {
+class InputWrapper extends Component {
+  static propTypes = {
+    navigator: PropTypes.object.isRequired
+  }
+
   render() {
     return (
-      <View style={styles.container}>
+      <View style={styles.wrapper}>
         <Input />
+      </View>
+    );
+  }
+}
+
+class PreviewWrapper extends Component {
+  static propTypes = {
+    navigator: PropTypes.object.isRequired
+  }
+
+  render() {
+    return (
+      <View style={styles.wrapper}>
         <Preview />
       </View>
     );
   }
 }
 
+class MarkdownMemo extends Component {
+  render() {
+    return (
+      <NavigatorIOS
+        ref='nav'
+        initialRoute={{
+          component: InputWrapper,
+          title: 'MarkdownMemo',
+          rightButtonTitle: 'Preview',
+          onRightButtonPress: () => this.refs.nav.push({
+            component: PreviewWrapper,
+            title: 'Preview'
+          }),
+          backButtonTitle: 'Back'
+        }}
+        barTintColor='#FF5722'
+        tintColor='#fff'
+        titleTextColor='#fff'
+        style={styles.container} />
+    );
+  }
+}
+
 const styles = StyleSheet.create({
   container: {
+    flex: 1
+  },
+  wrapper: {
     flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'flex-start',
+    flexDirection: 'column',
     backgroundColor: '#F5FCFF',
-  }
+    paddingTop: 64
+  },
 });
 
 AppRegistry.registerComponent('MarkdownMemo', () => MarkdownMemo);
